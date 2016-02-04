0c8feb194e6f4cbbab29b89db62f4795fcb1bf60
Joel Handwell <joelhandwell@gmail.com>
2016-02-04 16:24:33 -0500

    Allow simple syntax

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,3 +16,11 @@ RSpec.configure do |config|
   config.platform = 'ubuntu'
   config.version = '14.04'
 end
+
+def web_apps
+  [
+    { vhost: { main_domain: 'example.com' }, connection_domain: { varnish_domain: 'varnish.example.com' }, varnish: { purge_target: true}},
+    { vhost: { main_domain: 'no-varnish.com' }, connection_domain: { webapp_domain: 'webapp.no-varnish.com', db_domain: 'db.no-varnish.com' }},
+    { vhost: { main_domain: 'docroot-only-vhost.com',                              docroot: '/websites/dov'              }}
+  ]
+end
